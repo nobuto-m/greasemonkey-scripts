3263589655956ef36a96069918186f87e25ccdc6
Kees Cook <kees@outflux.net>
2007-10-17 16:24:50 -0700

    inspired by Soren's changes, rearranged to handle muliple data sources

--- lp_karma_suffix.user.js
+++ lp_karma_suffix.user.js
@@ -2,22 +2,23 @@
 // @name           LP_Karma_Suffix
 // @namespace      http://outflux.net/greasemonkey/
 // @description    (Launchpad) Karma Suffixes
-// @include        https://*.launchpad.net/*
 // @include        https://launchpad.net/*
-// @version        0.9
-// @date           2007-06-26
+// @include        https://*.launchpad.net/*
+// @include        https://*.edge.launchpad.net/*
+// @version        1.0
+// @date           2007-10-17
 // @creator        Kees Cook <kees@ubuntu.com>
 // ==/UserScript==
 
 (function () {
   var SCRIPT = {
     name: "LP_Karma_Suffix",
-    namespace: "http://outflux.net/greasemonkey/",
+    namespace: "https://code.launchpad.net/~ubuntu-dev/ubuntu-gm-scripts/ubuntu",
     description: '(Launchpad) Karma Suffixes',
-    source: "http://outflux.net/greasemonkey/",
-    identifier: "http://outflux.net/greasemonkey/lp_karma_suffix.user.js",
-    version: "0.9",
-    date: (new Date(2007, 6 - 1, 26))// update date
+    source: "http://codebrowse.launchpad.net/~ubuntu-dev/ubuntu-gm-scripts/ubuntu",
+    identifier: "http://codebrowse.launchpad.net/~ubuntu-dev/ubuntu-gm-scripts/ubuntu/lp_karma_suffix.user.js",
+    version: "1.0",
+    date: (new Date(2007, 10 - 1, 17))// update date
     .valueOf()
   };
 
@@ -73,82 +74,101 @@ function loadData(URL, response_handler, response_arg)
 }
 
 
-
+var debug = 0;
 var people_cache = new Array();
 
-function person_handler(xmldoc, person)
+// Actually populates the visible HTML with fetched information.
+// Called after team_handler and karma_handler have finished.
+function augment_person(person)
 {
-    //alert(stringObject(xmldoc,"xmldoc"));
-    //alert(stringObject(xmldoc.responseText,"xmldoc.responseText"));
-    //alert(stringObject(xmldoc.responseXML,"xmldoc.responseXML"));
-    //alert(xmldoc.responseXML);
+    for (var idx in people_cache[person]['nodes']) {
+         var node = people_cache[person]['nodes'][idx];
 
-/*
-    It seems that the output of LP can't be used as an XML document,
-    so we're forced to build a screen-scraper!!  (LP #92853)
+        // show developer team logos last, so process them first
+        for (var title in people_cache[person]['team']) {
+            var logoNode = document.createElement('img');
+            logoNode.title = title;
+            logoNode.src = people_cache[person]['team'][title];
+            node.parentNode.insertBefore(logoNode, node.nextSibling);
+        }
+        
+        // add in karma
+        var karmaNode = document.createTextNode(' ('+people_cache[person]['karma']+') ');
+        node.parentNode.insertBefore(karmaNode, node.nextSibling);
+    }
+}
 
-//    var results = xpath("//span[contains(@id,'karma-total')]", xmldoc);
-//    var node = results.snapshotItem(0);
-    var node = xmldoc.getElementById('karma-total');
-    var karma = node.firstChild.nodeValue;
-*/
+
+// Handles extracting the karma details
+function karma_handler(xmldoc, person)
+{
+    // The output of LP can't be used as an XML document, so we're forced
+    // to build a screen-scraper!!  (LP #92853)
     var text = xmldoc.responseText.replace(/\n/g," ");
 
-    // skip non-humans
-    if (text.indexOf('Show all members')>=0) {
-        return;
-    }
-
-    /* show developer team logos last, so process them first */
-    var teams = {
-	'motu-swat':'',
-	'bugsquad':'',
-	'ubuntu-core-dev':'',
-	'ubuntu-dev':'',
-	'ubuntu-qa':'',
-	'ubuntumembers':''
-    };
-    var re = new RegExp("<img[^>]* src=\"(https://launchpadlibrarian.net/[^\"]+)\"[^>]+>[^a]*<a[^>]* href=\"[^~\"]*/~([^\"]+)\"","ig");
-    while ((match = re.exec(text)) != null) {
-	if (match[2] in teams) {
-	    //alert(match[2]+' is '+match[1]+' for '+person);
-	    for (var idx in people_cache[person]['nodes']) {
-		var node = people_cache[person]['nodes'][idx];
-		var logoNode = document.createElement('img');
-		logoNode.src = match[1];
-		logoNode.title = match[2];
-		node.parentNode.insertBefore(logoNode, node.nextSibling);
-	    }
-	}
-    }
-
-    /* add in karma */
     var karma = '0';
-/* XPath method... 
-    var results = xpath("//span[contains(@id,'karma-total')]", xmldoc.responseXML);
-    if (results.snapshotLength>0) {
-        karma = results.snapshotItem(0).firstChild.nodeValue;
-        alert("Karma for "+person+": "+karma);
-    }
-*/
-// screen-scraping method...
+
+    // XPath method... 
+    //var results = xpath("//span[contains(@id,'karma-total')]", xmldoc.responseXML);
+    //if (results.snapshotLength>0) {
+    //    karma = results.snapshotItem(0).firstChild.nodeValue;
+    //    //alert("Karma for "+person+": "+karma);
+    //}
+
+    // screen-scraping method...
     var span = '<span id="karma-total">';
     var karma_at = text.indexOf(span);
     if (karma_at>0) {
         var karma_all = text.substr(karma_at+span.length);
         karma = karma_all.substr(0,karma_all.indexOf("<"));
-        //alert("Karma for "+person+": "+karma);
+        if (debug)
+            alert("Karma for "+person+": "+karma);
     }
 
-    for (var idx in people_cache[person]['nodes']) {
-         var node = people_cache[person]['nodes'][idx];
+    // store for later
+    people_cache[person]['karma'] = karma;
 
-         var karmaNode = document.createTextNode(' ('+karma+') ');
-         node.parentNode.insertBefore(karmaNode, node.nextSibling);
-    }
+    // end of chain, display
+    augment_person(person);
 }
 
-function add_people(people, debug)
+// Handles extracting the team details
+function team_handler(xmldoc, person)
+{
+    // The output of LP can't be used as an XML document, so we're forced
+    // to build a screen-scraper!!  (LP #92853)
+    var text = xmldoc.responseText.replace(/\n/g," ");
+
+    // skip non-humans (this will abort the fetching chain)
+    if (text.indexOf('Show all members')>=0) {
+        return;
+    }
+
+    // show developer team logos last, so process them first
+    var teams = {
+        'motu-swat':'',
+        'bugsquad':'',
+        'ubuntu-core-dev':'',
+        'ubuntu-dev':'',
+        'ubuntu-qa':'',
+        'ubuntumembers':''
+    };
+    var re = new RegExp("<img[^>]* src=\"(https://launchpadlibrarian.net/[^\"]+)\"[^>]+>[^a]*<a[^>]* href=\"[^~\"]*/~([^\"]+)\"","ig");
+    while ((match = re.exec(text)) != null) {
+        title = match[2];
+        src = match[1];
+        if (title in teams) {
+            people_cache[person]['team'][title] = src;
+            if (debug)
+                alert(title+' has URL '+src+' for '+person);
+        }
+    }
+
+    // chain to next item to load...
+    loadData(people_cache[person]['karma_link'], karma_handler, person);
+}
+
+function add_people(people)
 {
   for (var i = 0; i < people.snapshotLength; i++) {
     var node = people.snapshotItem(i);
@@ -167,9 +187,12 @@ function add_people(people, debug)
 
     if (!people_cache[person]) {
         people_cache[person] = new Array();
-        people_cache[person]['link'] = link + "/+participation";
+        people_cache[person]['team_link'] = link + "/+participation";
+        people_cache[person]['karma_link'] = link + "/@@+portlet-details";
+        people_cache[person]['email_link'] = link + "/@@+portlet-email";
         people_cache[person]['nodes'] = new Array();
-        if (debug) 
+        people_cache[person]['team'] = new Array();
+        if (debug)
             alert(person);
     }
     people_cache[person]['nodes'].push(node);
@@ -179,6 +202,8 @@ function add_people(people, debug)
 window.addEventListener("load", function(e) {
     var prefix = new Array("https://launchpad.net",
                            "https://bugs.launchpad.net",
+                           "https://edge.launchpad.net",
+                           "https://bugs.edge.launchpad.net",
                            "");
     var url_clean_matches = new Array();
     var url_messy_matches = new Array();
@@ -201,9 +226,9 @@ window.addEventListener("load", function(e) {
     // Everyone!  (this is totally insane for bugs with large dups)
     //add_people(xpath("//"+a_match));
 
-    // Go fetch all found people's karma and attach it to their nodes
+    // Go fetch all found people's info and attach it to their nodes
     for (var person in people_cache) {
-        loadData(people_cache[person]['link'], person_handler, person);
+        loadData(people_cache[person]['team_link'], team_handler, person);
     }
 
 }, false);
