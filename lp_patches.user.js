5fc85954e91711aa02974a33fe9357661c988ea9
Brian Murray <brian@canonical.com>
2008-06-20 10:26:27 -0700

    lp_patches.user.js: identify patches in bug comments and updated README

--- lp_patches.user.js
+++ lp_patches.user.js
@@ -21,16 +21,16 @@ function xpath(query, context) {
                             XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
 }
 
-function requestHandler(req, fn, args)
+function requestHandler(req, fn, args, arg2)
 {
     if (req.readyState == 4 && req.status == 200) {
-        fn(req, args);
+        fn(req, args, arg2);
         //fn(req.responseXML, args);
         //fn(req.responseXML.documentElement, args);
     }
 }
 
-function loadData(URL, response_handler, response_arg)
+function loadData(URL, response_handler, response_arg, response_arg2)
 {
     //alert("Fetching " + response_arg + " ("+URL+")");
 
@@ -56,7 +56,7 @@ function loadData(URL, response_handler, response_arg)
     }
 
     // Anonymous function to handle changed request states
-    httpRequest.onreadystatechange = function() { requestHandler(httpRequest, response_handler, response_arg); };
+    httpRequest.onreadystatechange = function() { requestHandler(httpRequest, response_handler, response_arg, response_arg2); };
 
     // Make the request
     httpRequest.open ('GET', URL);
@@ -65,27 +65,43 @@ function loadData(URL, response_handler, response_arg)
     httpRequest.send (null);
 }
 
-function patch_handler(xmldoc, edit_link) {
+function patch_handler(xmldoc, librarian_link, comment) {
     var text = xmldoc.responseText.replace(/\n/g," ");
 
 //    if (xpath("//input[contains(@name,'field.patch') and contains(@value,'on')]", xmldoc.responseText)) {
     var re = new RegExp("checked=\"checked\" id=\"field.patch\"");
     if ((match = re.exec(text)) != null) {
-        if (debug)
-            GM_log("Patch found " + edit_link);
-        edit_link.parentNode.parentNode.className = "news";
+        if (debug) {
+            GM_log("Patch found " + librarian_link);
+        }
+        librarian_link.parentNode.className = "news";
+        for (var j = 0; j < comment.snapshotLength; j++) {
+            if (debug) 
+                GM_log("Comment url " + comment.snapshotItem(j));
+            if ( String(librarian_link) == String(comment.snapshotItem(j))) {
+                comment.snapshotItem(j).parentNode.className = "news";
+            }
+        } 
+        
     }
 }
 
 window.addEventListener("load", function(e) {
 //    GM_log('script running');
 
-    var edit = xpath("//div[contains(@id,'portlet-attachments')]//li[contains(@class,'download')]/small/a");
-    for (var i = 0; i < edit.snapshotLength; i++) {
+    var librarian = xpath("//div[contains(@id,'portlet-attachments')]//li[contains(@class,'download')]/a");
+    var comment = xpath("//div[contains(@class,'boardCommentBody')]//ul//li[contains(@class,'download')]/a");
+    if (debug) {
+        GM_log("comment is "+comment);
+        GM_log("librarian is "+librarian);
+    }
+    for (var i = 0; i < librarian.snapshotLength; i++) {
+        var librarian_link = librarian.snapshotItem(i);
+        var edit = xpath("//div[contains(@id,'portlet-attachments')]//li[contains(@class,'download')]/small/a");
         if (debug)
             GM_log("edit url "+edit.snapshotItem(i));
         var edit_link = edit.snapshotItem(i);
-        loadData(edit_link, patch_handler, edit_link);
+        loadData(edit_link, patch_handler, librarian_link, comment);
     }
 
   }, false);
