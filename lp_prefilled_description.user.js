dc1b4bc3a422a210bc24fe88bc75ea8cdf558e3c
Brian Murray <brian@canonical.com>
2009-02-11 11:19:05 -0800

    lp_prefilled_description.user.js: adding a prefilled description thanks to cwayne for the idea

---
+++ lp_prefilled_description.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name           LP_DetailedDescription
+// @namespace      http://murraytwins.com/greasemonkey/
+// @description    (Launchpad) Prefill bug description
+// @include        https://launchpad.net/*
+// @include        https://*.launchpad.net/*
+// @include        https://*.edge.launchpad.net/*
+// @date           2009-02-11
+// @creator        Brian Murray <brian@ubuntu.com>
+// ==/UserScript==
+// This would be more awesome if worked like buttontags
+// and you could have different descriptions for different
+// projects
+
+
+(function() {
+
+// Description to use
+var description = "Release of Ubuntu:\nPackage Version:\nExpected Results:\nActual Results:\n"
+
+// ------- End of User settable data -------
+
+function xpath(query, context) {
+    context = context ? context : document;
+    return document.evaluate(query, context, null,
+                            XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+}
+
+window.addEventListener("load", function(e) {
+
+    // grab the part after bugs.launchpad.net - pathname looks like /ubuntu/+source/pkgname/+bug/1
+    var pathname = window.location.pathname;
+    // find the project name
+    var project_name = pathname.split('/')[1]
+    // project for the description to apply to
+    if ( project_name == 'ubuntu' ) {
+        if (xpath('//form[contains(@action,"filebug")]').snapshotItem(0)) {
+            xpath('//textarea[@id="field.comment"]').snapshotItem(0).value = description;
+        }
+    }
+  }, false);
+})(); 
