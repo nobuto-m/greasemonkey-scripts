c3348fb63e9e3f98239498f7c37155a3cf94b9a2
Brian Murray <brian@canonical.com>
2009-06-29 15:48:38 -0700

    lp_buttontags.user.js: rewriting buttontags to load tags from an xml file to allow for easier updating

--- lp_buttontags.user.js
+++ lp_buttontags.user.js
@@ -7,67 +7,6 @@
 // @include        https://bugs.staging.launchpad.net/*
 // ==/UserScript==
 
-// ------  User settable data  -------------
-
-// List of tags to display in the UI
-var tags = new Object;
-tags = [
-        // --- Ubuntu Section ---
-        // ubuntu template
-        // {"tag":'', tip:"", "project":"ubuntu", "package":""},
-        //
-        // any package - source https://wiki.ubuntu.com/Bugs/Tags
-        {"tag":'a11y', tip:"An accessibility problem", "project":"ubuntu", "package":"any"},
-        {"tag":'bitesize', tip:"Probably an easy fix, appropriate for new developers", "project":"ubuntu", "package":"any"},
-        {"tag":'hw-specifc', tip:"Requires specfic hardware to recreate", "project":"ubuntu", "package":"any"},
-        {"tag":'likely-dup', tip:"Sounds like a dupe of an existing bug; needs further investigation", "project":"ubuntu", "package":"any"},
-        {"tag":'metabug', tip:"High probability of duplicate reports", "project":"ubuntu", "package":"any"},
-        {"tag":'needs-packaging', tip:"A request for software to be packaged", "project":"ubuntu", "package":"any"},
-        {"tag":'notifications', tip:"Related to Jaunty+ notification system", "project":"ubuntu", "package":"any"},       
-        {"tag":'packaging', tip:"Strictly a packaging issue", "project":"ubuntu", "package":"any"},
-        {"tag":'regression-potential', tip:"Bug in devel release and not stable", "project":"ubuntu", "package":"any"},
-        {"tag":'regression-release', tip:"Bug in stable release and not previous stable release", "project":"ubuntu", "package":"any"},
-        {"tag":'screencast', tip:"Screencast of the bug is attached", "project":"ubuntu", "package":"any"},
-        {"tag":'string-fix', tip:"For spelling and grammatic errors", "project":"ubuntu", "package":"any"},
-        {"tag":'usability', tip:"A usability issue with the application", "project":"ubuntu", "package":"any"},
-        {"tag":'ubuntu-unr', tip:"Netbook remix issue", "project":"ubuntu", "package":"any"},
-        // package openoffice - source https://wiki.ubuntu.com/DebuggingOpenOffice
-        {"tag":'ooo-base', tip:"Bug report about database program", "project":"ubuntu", "package":"openoffice"},
-        {"tag":'ooo-calc', tip:"Bug report about spreadsheet application", "project":"ubuntu", "package":"openoffice"},
-        {"tag":'ooo-impress', tip:"Bug report about presentation application", "project":"ubuntu", "package":"openoffice"},
-	{"tag":"ooo-writer", tip:"Bug report about word processor", "project":"ubuntu", "package":"openoffice"},
-        // package update-manager - source https://wiki.ubuntu.com/DebuggingUpdateManager
-	{"tag":"cdrom-upgrade", tip:"Related to an upgrade from CD-ROM or DVD media", "project":"ubuntu", "package":"update-manager"},
-        {"tag":'dapper2hardy', tip:"Related to an upgrade from Dapper to Hardy", "project":"ubuntu", "package":"update-manager"},
-        {"tag":'edgy2feisty', tip:"Related to an upgrade from Edgy to Feisty", "project":"ubuntu", "package":"update-manager"},
-        {"tag":'feisty2gutsy', tip:"Related to an upgrade from Feisty to Gutsy", "project":"ubuntu", "package":"update-manager"},
-        {"tag":'gutsy2hardy', tip:"Related to an upgrade from Gutsy to Hardy", "project":"ubuntu", "package":"update-manager"},
-        {"tag":'hardy2intrepid', tip:"Related to an upgrade from Hardy to Intrepid", "project":"ubuntu", "package":"update-manager"},
-        {"tag":'intrepid2jaunty', tip:"Related to an upgrade from Intrepid to Jaunty", "project":"ubuntu", "package":"update-manager"},
-        // package linux - source https://wiki.ubuntu.com/Bugs/Tags 
-        // it'd be neat if they applied to other kernel packages too...
-	{"tag":"cherry-pick", tip:"Has a git commit SHA from upstream", "project":"ubuntu", "package":"linux"},
-        {"tag":"kernel-bug", tip:"BUG: message appears in logs", "project":"ubuntu", "package":"linux"},
-        {"tag":"kernel-oops", tip:"Causes a kernel Oops", "project":"ubuntu", "package":"linux"},
-        // package network-manager - source https://wiki.ubuntu.com/DebuggingNetworkManager
-        {"tag":"driver-madwifi", tip:" The madwifi driver is in use", "project":"ubuntu", "package":"network-manager"},
-        {"tag":"driver-ndiswrapper", tip:" The ndiswrapper driver is in use", "project":"ubuntu", "package":"network-manager"},
-        {"tag":"encryption-wep", tip:" WEP encryption is used", "project":"ubuntu", "package":"network-manager"},
-        {"tag":"encryption-wpa", tip:" WPA encryption is used", "project":"ubuntu", "package":"network-manager"},
-        {"tag":"encryption-wpa2", tip:" WPA2 encryption is used", "project":"ubuntu", "package":"network-manager"},
-        {"tag":"vpn", tip:"Related to either openvpn, vpnc or pptp network-manager vpn modules", "project":"ubuntu", "package":"network-manager"},
-
-        // --- Projects Section ---
-        // project template
-        // {"tag":'', tip:"", "project":""},
-        //
-        // project - malone source https://dev.launchpad.net/LaunchpadBugTags
-        {"tag":"api", tip:"Related to any machine readable output", "project":"malone"},
-        {"tag":"bugwatch", tip:"Related to bug watches", "project":"malone"}
-];
-
-// ------- End of User settable data -------
-
 // Feature Wishlist:
 //   * Clean up the code (needs some functions broken out)
 //   * Reliable way for users to add new tags, rather than editing the code above
@@ -80,6 +19,31 @@ tags = [
 //     rather slow.  However, the description in the main bug page is full
 //     of HTML markup tags.  Perhaps it could be parsed to remove the markup
 //     so that it's equivalent to what would have been in +edit's textarea?
+
+
+
+(function () {
+  var SCRIPT = {
+    name: "LP_ButtonTags",
+    namespace: "http://bryceharrington.org/greasemonkey/",
+    description: '(Launchpad) Buttons for adding tags',
+    source: "http://bryceharrington.org/greasemonkey/",
+    identifier: "http://bryceharrington.org/greasemonkey/lp_buttontags.user.js",
+    version: "0.9.0",
+    date: (new Date(2009, 6 - 1, 29))// update date
+    .valueOf()
+  };
+
+var debug = 0;
+
+var tags = new Object;
+
+var tagFields = new Array(
+                            "name",       // required -- name of the tag to use
+                            "tip",        // required -- the tooltip to show on mouse over
+                            "project",    // required -- the project the tag should be displayed for 
+                            "package"     // required -- the package the tag should be displayed for
+                          );
 
 // Sends data to url using HTTP POST, then calls the function cb with the response text as its single argument.
 function post(url, data, cb) {
@@ -122,61 +86,165 @@ function get(url, cb) {
   });
 }
 
-// Does document already have list of Tags?  If so, snag them.
-var tags_current = document.getElementById('tag-list');
-var tags_current_list = new Array();
-if (tags_current) {
-	var tags_current_anchors = tags_current.getElementsByTagName("a");
-	// Extract text from child <a> tags and put into an array
-	for (var i = 0; i < tags_current_anchors.length; i++) {
-		tags_current_list[tags_current_list.length] = tags_current_anchors[i].innerHTML;
-	}
-	//alert(tags_current_list.join(" "));
+// grab the standard tags used from an xml file
+function loadStandardTags() {
+    if (debug) {
+        alert("loadStandardTags");
+    }
+    GM_xmlhttpRequest
+        (
+          {
+            method: 'GET',
+            url:    'http://people.ubuntu.com/~brian/greasemonkey/bugsquad-tags.xml',
+            headers: {
+                'Accept': 'application/atom+xml,application/xml,text/xml',
+            },
+            onload:  function(results) {
+                if (debug) {
+                    alert("loadStandardTags onload start");
+                }
+                var parser = new DOMParser();
+                var dom = parser.parseFromString(results.responseText,"application/xml");
+                var definedTags = dom.getElementsByTagName('tag');
+                /* if we actually have some replies, clear the old ones */
+                if (definedTags.length > 0) {
+                    tags.count = 0;
+                }
+                var base = 0;
+                for (var i=0; i < definedTags.length; i++) {
+                    for (var field in tagFields) {
+                        var fieldname = tagFields[field];
+                        //tags[fieldname] = new Array;
+                        var text = definedTags[i].getElementsByTagName(fieldname)[0].textContent;
+                        tags[fieldname][base+i] = text;
+                    }
+                }
+                tags.count += definedTags.length
+                // reload again in 1.5 days
+                var time = new Date();
+                tags.reloadAt = time.getUTCMilliseconds() + (1000 * 60 * 60 * 36);
+                savePreferences();
+                // run through the magical display tags function again
+                displayTags();
+                if (debug) {
+                    alert("loadStandardTags onload end");
+                }
+            }
+          }
+        )
 }
 
-var tag_section = document.createElement("div");
-tag_section.setAttribute('style', "text-align: right; margin-bottom: 1em;");
+function loadPreferences()
+{
+    if (debug) {
+        alert("loadPreferences start");
+    }
+    tags.standardSeen = false;
+    tags.count = parseInt(GM_getValue('count', 0));
+    for (var field in tagFields) {
+        var fieldname = tagFields[field];
+        tags[fieldname] = new Array;
 
-var innerTextElement = document.createTextNode("Add tag: ");
-tag_section.appendChild(innerTextElement);
-
-// grab the part after bugs.launchpad.net - pathname looks like /ubuntu/+source/pkgname/+bug/1
-var pathname = window.location.pathname;
-// find the project name
-var project_name = pathname.split('/')[1]
-// find the source package name
-var package_name = pathname.split('/')[3]
-
-tags:
-for (var tag in tags) {
-	// Skip if tag is already listed
-	for (var current_tag in tags_current_list) {
-		if (tags_current_list[current_tag] == tags[tag]["tag"]) {
-			continue tags;
-		}
-	}
-
-        // Skip if tag is not part of the project
-        if ( tags[tag]["project"] != project_name ) {
-            continue tags;
+        for (var idx = 0; idx < tags.count; idx ++) {
+            tags[fieldname][idx] = GM_getValue(fieldname+idx,"");
         }
+    }
+    tags.reloadAt = parseInt(GM_getValue('reload-at', 0));
+    if (debug) {
+        alert("loadPreferences finish");
+    }
+}
 
-        // if the project is ubuntu check the package name that the tag applies to
-        if ( project_name == 'ubuntu' ) {
-            // Skip if tag isn't for this package or for the catch-all any package
-            if ( (tags[tag]["package"] != pathname.split('/')[3]) && (tags[tag]["package"] != 'any') ) {
+function savePreferences()
+{
+    if (debug) {
+        alert("savePreferences start");
+    }
+    // save the count
+    GM_setValue('count', ''+tags.count);
+    // save standard-reply-reload date
+    GM_setValue('reload-at', ''+tags.reloadAt);
+
+    // save the preferences
+    for (var idx = 0; idx < tags.count; idx ++) {
+        for (var field in tagFields) {
+            //alert("Saving "+tagFields[field]+idx);
+            GM_setValue(tagFields[field]+idx, tags[tagFields[field]][idx]);
+        }
+    }
+
+    if (debug) {
+        alert("savePreferences end");
+    }
+}
+
+function displayTags()
+{
+    if (debug) {
+        alert("displayTags start");
+    }
+    // Does document already have list of Tags?  If so, snag them.
+    var tags_current = document.getElementById('tag-list');
+    var tags_current_list = new Array();
+    if (tags_current) {
+	    var tags_current_anchors = tags_current.getElementsByTagName("a");
+	    // Extract text from child <a> tags and put into an array
+	    for (var i = 0; i < tags_current_anchors.length; i++) {
+		    tags_current_list[tags_current_list.length] = tags_current_anchors[i].innerHTML;
+	    }
+	    //alert(tags_current_list.join(" "));
+    }
+
+    var gmtags = document.getElementById('gm-tags');
+    if (gmtags) {
+        gmtags.parentNode.removeChild(gmtags);
+    }
+
+    var tag_section = document.createElement("div");
+    tag_section.setAttribute('style', "text-align: left; margin-bottom: 1em;");
+    tag_section.setAttribute('id', "gm-tags");
+    
+    var innerTextElement = document.createTextNode("Add tag: ");
+    tag_section.appendChild(innerTextElement);
+    
+    // grab the part after bugs.launchpad.net - pathname looks like /ubuntu/+source/pkgname/+bug/1
+    var pathname = window.location.pathname;
+    // find the project name
+    var project_name = pathname.split('/')[1]
+    // find the source package name
+    var package_name = pathname.split('/')[3]
+
+    tags:
+    for (var idx = 0; idx < tags.count; idx++) {
+    	// Skip if tag is already listed
+    	for (var current_tag in tags_current_list) {
+                    if (tags_current_list[current_tag] == tags["name"][idx]) {
+    			continue tags;
+    		}
+    	}
+    
+            // Skip if tag is not part of the project
+            if ( tags["project"][idx] != project_name ) {
                 continue tags;
+                
             }
-        }
-
-	var tag_button = document.createElement("a");
-	tag_button.href = "#" + tags[tag]["tag"];
-	tag_button.id = tags[tag]["tag"];
-	tag_button.title = tags[tag]["tip"];
-	tag_button.innerHTML = " " + tags[tag]["tag"] + " ";
-	tag_button.addEventListener('click', function(event) {
-		event.preventDefault();
-//		event.stopPropagation(); // not sure if this is needed
+    
+            // if the project is ubuntu check the package name that the tag applies to
+            if ( project_name == 'ubuntu' ) {
+                // Skip if tag isn't for this package or for the catch-all any package
+                if ( (tags["package"][idx] != pathname.split('/')[3]) && (tags["package"][idx] != 'any') ) {
+                    continue tags;
+                }
+            }
+    
+    	var tag_button = document.createElement("a");
+    	tag_button.href = "#" + tags["name"][idx];
+    	tag_button.id = tags["name"][idx];
+    	tag_button.title = tags["tip"][idx];
+    	tag_button.innerHTML = " " + tags["name"][idx] + " ";
+    	tag_button.addEventListener('click', function(event) {
+    		event.preventDefault();
+      		//event.stopPropagation(); // not sure if this is needed
 
 		if (!GM_xmlhttpRequest) {
 			alert('This functionality requires a newer version of Greasemonkey - please upgrade!');
@@ -189,7 +257,7 @@ for (var tag in tags) {
 		get(document.location + "/+edit", function(responseText) {
 			//alert("Received responseText");
 
-		    // Get the old details
+		        // Get the old details
 			var xmlobject = (new DOMParser()).parseFromString(responseText, "text/xml");
 
 			var bug_description = xmlobject.getElementById('field.description').textContent;
@@ -230,12 +298,39 @@ for (var tag in tags) {
 	}, false);
 
 	tag_section.appendChild(tag_button);
+    }
+    // adding in a reload button
+    var reload_button = document.createElement("a");
+    reload_button.href = "#[+reload+]";
+    reload_button.id = "[+reload+]";
+    reload_button.innerHTML = " [+reload+] ";
+    reload_button.addEventListener('click', function(event) { event.preventDefault(); loadStandardTags(); alert('Tags Loaded'); return false;}, false);
+
+    tag_section.appendChild(reload_button);
+
+    var bug_desc = document.getElementById('bug-description');
+    if (bug_desc) {
+        bug_desc.parentNode.insertBefore(tag_section, bug_desc);
+    }
+    if (debug) {
+        alert("displayTags end");
+    }
 }
 
-var bug_desc = document.getElementById('bug-description');
+window.addEventListener("load", function(e) {
 
-bug_desc.parentNode.insertBefore(tag_section, bug_desc);
+	loadPreferences();
+	// load tags if not already in the preferences, or
+	// if the "reloadAt" preference has expired
+	var time = new Date();
 
-//alert(tag_section.innerHTML);
+	if (time.getUTCMilliseconds() > tags.reloadAt) {
+	    loadStandardTags();
+	}
+	else {
+	    displayTags();
+	} 
 
+}, false);    
 
+})();
