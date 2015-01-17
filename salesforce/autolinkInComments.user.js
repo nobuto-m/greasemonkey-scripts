36a52133ab054bcb67901ebe5c88d3d3dffdc87a
Nobuto MURATA <nobuto@ubuntu.com>
2015-01-18 01:38:29 +0900

    bump version to use updated jQuery-linkify
    Fixes #2

--- salesforce/autolinkInComments.user.js
+++ salesforce/autolinkInComments.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/nobuto-m/greasemonkey-scripts/tree/master/salesforce
 // @updateURL   https://github.com/nobuto-m/greasemonkey-scripts/raw/master/salesforce/autolinkInComments.user.js
 // @match       https://eu1.salesforce.com/*
-// @version     1.4
+// @version     1.5
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @require     https://github.com/nobuto-m/greasemonkey-scripts/raw/master/libs/jQuery-linkify/dist/jquery.linkify.min.js
