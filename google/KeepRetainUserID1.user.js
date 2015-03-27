ee7f14e0d41f518bf3a5a08f15ce1af9848e4f3e
Nobuto Murata <nobuto@ubuntu.com>
2015-03-27 15:25:35 +0900

    retain hash as well

--- google/KeepRetainUserID1.user.js
+++ google/KeepRetainUserID1.user.js
@@ -3,8 +3,11 @@
 // @description Retain user ID 1 in URL to show the same user's content on reload or session recovery
 // @namespace   https://github.com/nobuto-m/greasemonkey-scripts/tree/master/google
 // @updateURL   https://github.com/nobuto-m/greasemonkey-scripts/raw/master/google/KeepRetainUserID1.js
-// @match       https://keep.google.com/u/1/
+// @match       https://keep.google.com/u/1/*
 // @version     1
 // @grant       none
 // ==/UserScript==
-history.replaceState(null, '', '/u/1/');
+var user_id = 1;
+var hash = window.location.hash;
+var pathname = '/u/' + user_id + '/' + hash;
+history.replaceState(null, '', pathname);
