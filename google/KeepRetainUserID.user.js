824a6aa92bc142b0c4e7e2c5648ab7fe2fc0b53d
Nobuto Murata <nobuto@ubuntu.com>
2016-04-09 19:19:26 +0900

    Update and rename KeepRetainUserID1.user.js to KeepRetainUserID.user.js

--- google/KeepRetainUserID.user.js
+++ google/KeepRetainUserID.user.js
@@ -2,7 +2,7 @@
 // @name        Google Keep - Retain user ID in URL
 // @description Retain user ID in URL to show the same user's content on reload or session recovery
 // @namespace   https://github.com/nobuto-m/greasemonkey-scripts/tree/master/google
-// @updateURL   https://github.com/nobuto-m/greasemonkey-scripts/raw/master/google/KeepRetainUserID1.js
+// @updateURL   https://github.com/nobuto-m/greasemonkey-scripts/raw/master/google/KeepRetainUserID.js
 // @match       https://keep.google.com/u/*
 // @version     1.1
 // @grant       none
