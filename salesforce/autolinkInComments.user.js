50730d010e2e3063ccddff0a8226695fe20a288d
Nobuto Murata <nobuto@ubuntu.com>
2017-09-29 01:08:27 +0900

    Let's drop internal dependency

--- salesforce/autolinkInComments.user.js
+++ salesforce/autolinkInComments.user.js
@@ -4,12 +4,12 @@
 // @namespace   https://github.com/nobuto-m/greasemonkey-scripts/tree/master/salesforce
 // @updateURL   https://github.com/nobuto-m/greasemonkey-scripts/raw/master/salesforce/autolinkInComments.user.js
 // @match       https://*.salesforce.com/*
-// @version     1.7
+// @version     1.8
 // @grant       none
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
-// @require     https://raw.githubusercontent.com/nobuto-m/greasemonkey-scripts/master/libs/linkify-shim/linkify.min.js
-// @require     https://raw.githubusercontent.com/nobuto-m/greasemonkey-scripts/master/libs/linkify-shim/linkify-string.min.js
-// @require     https://raw.githubusercontent.com/nobuto-m/greasemonkey-scripts/master/libs/linkify-shim/linkify-jquery.min.js
+// @require     https://raw.githubusercontent.com/nfrasser/linkify-shim/67ecf680a10e3ed029e4bfd62e8217e27326a6ae/linkify.min.js
+// @require     https://raw.githubusercontent.com/nfrasser/linkify-shim/67ecf680a10e3ed029e4bfd62e8217e27326a6ae/linkify-string.min.js
+// @require     https://raw.githubusercontent.com/nfrasser/linkify-shim/67ecf680a10e3ed029e4bfd62e8217e27326a6ae/linkify-jquery.min.js
 // ==/UserScript==
 
 var comments = $('div[id$=\'_RelatedCommentsList_body\']').find('td.dataCell');
