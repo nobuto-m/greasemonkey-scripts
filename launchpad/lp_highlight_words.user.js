7548b90493aa7dabd4103c40e62a69df8da7b1d9
Nobuto Murata <nobuto@ubuntu.com>
2017-10-26 23:58:24 +0900

    update supported releases

--- launchpad/lp_highlight_words.user.js
+++ launchpad/lp_highlight_words.user.js
@@ -26,9 +26,9 @@ function xpath(query, context) {
 {
     // Regex of words to highlight : color to highlight them (hint don't use black!)
     var color_map = {
-        'DistroRelease: [^ ]+ [^ ]+|SourcePackage: [^ ]+|LiveMediaBuild:|zesty|yakkety|xenial|trusty|precise': "yellow",  // things that might be helpful
+        'DistroRelease: [^ ]+ [^ ]+|SourcePackage: [^ ]+|LiveMediaBuild:|bionic|artful|zesty|xenial|trusty': "yellow",  // things that might be helpful
         'regression': "red", // critical importance
-        'wily|vivid|utopic|saucy|raring|quantal|oneiric|natty|maverick|lucid|karmic|jaunty|intrepid|hardy|gutsy|feisty|edgy|dapper|breezy|hoary|warty': "orange", // unsupported releases
+        'yakkety|wily|vivid|utopic|saucy|raring|quantal|precise|oneiric|natty|maverick|lucid|karmic|jaunty|intrepid|hardy|gutsy|feisty|edgy|dapper|breezy|hoary|warty': "orange", // unsupported releases
         // SRU verification tags
         'verification-needed': "yellow",
         'verification-done': "lime",
